56130fb3200dce0198d1ca2650d8deff90cde83d
uoouu <88788181+uoouu@users.noreply.github.com>
2022-04-12 19:23:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ timenh = time.strftime('%I')
 timenh = int(timenh)+3
 timenm = time.strftime('%M')
 ttt = f'{timenh}:{timenm}'
-requests.post(f"https://api.telegram.org/bot5200214331:AAGBoBxoxS9IQVO24rI0ZK-REvsd_IpnDOo/editmessagetext?chat_id=1110953194&message_id=271&text="'Bot start at '+timeo+'\n'+'time now is '+ttt)
+def  rer():
+    requests.post(f"https://api.telegram.org/bot5200214331:AAGBoBxoxS9IQVO24rI0ZK-REvsd_IpnDOo/editmessagetext?chat_id=1110953194&message_id=271&text="'Bot start at '+timeo+'\n'+'time now is '+ttt)
 
-
+while True:
+    print("POT IS RUNNNN")
+    rer()

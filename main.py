95d057f81cf82b0864b37c5b35144408afd03438
uoouu <88788181+uoouu@users.noreply.github.com>
2022-04-12 19:59:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ timenh = time.strftime('%I')
 timenh = int(timenh)+3
 timenm = time.strftime('%M')
 ttt = f'{timenh}:{timenm}'
+gad = 0
+
 def  rer():
     gg = requests.post(f"https://api.telegram.org/bot5200214331:AAGBoBxoxS9IQVO24rI0ZK-REvsd_IpnDOo/editmessagetext?chat_id=1110953194&message_id=271&text="'Bot start at '+timeo+'\n'+'time now is '+ttt)
     print(gg.text)
-gad = 0
+
 while True:
     time.sleep(1)
     gad +=1

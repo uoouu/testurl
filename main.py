5ed41805d01cafbd3ca8d3426517055801bc4b21
uoouu <88788181+uoouu@users.noreply.github.com>
2022-04-12 19:08:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,4 @@ timenm = time.strftime('%M')
 ttt = f'{timenh}:{timenm}'
 requests.post(f"https://api.telegram.org/bot5200214331:AAGBoBxoxS9IQVO24rI0ZK-REvsd_IpnDOo/editmessagetext?chat_id=1110953194&message_id=271&text="'Bot start at '+timeo+'\n'+'time now is '+ttt)
 
-app = Flask(__name__)
 
-@app.route('/')
-def red():
-    return(redirect('/get-time'))
-
-@app.route("/get-time", methods=['GET'])
-
-def tt():
-    return('Bot start at '+timeo+'<br/><br/>'+'time now is '+ttt)
-
-if __name__ == '__main__':
-    app.run()

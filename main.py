4eed5941ec8f01cb41d21e75133ad966a78b9198
uoouu <88788181+uoouu@users.noreply.github.com>
2022-04-12 19:24:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ def  rer():
     requests.post(f"https://api.telegram.org/bot5200214331:AAGBoBxoxS9IQVO24rI0ZK-REvsd_IpnDOo/editmessagetext?chat_id=1110953194&message_id=271&text="'Bot start at '+timeo+'\n'+'time now is '+ttt)
 
 while True:
-    print("POT IS RUNNNN")
+    print("BOT IS RUNNNN")
     rer()

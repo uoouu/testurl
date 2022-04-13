85426ff9f2989a6bfb3785ae3dbe05b6439c3607
uoouu <88788181+uoouu@users.noreply.github.com>
2022-04-13 14:40:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ ttt = f'{timenh}:{timenm}'
 gad = 0
 
 def  rer():
-    gg = requests.post(f"https://api.telegram.org/bot5200214331:AAGBoBxoxS9IQVO24rI0ZK-REvsd_IpnDOo/editmessagetext?chat_id=1110953194&message_id=271&text="'Bot start at '+timeo+'\n'+'time now is '+ttt)
+    gg = requests.post(f"https://api.telegram.org/bot/editmessagetext?chat_id=1110953194&message_id=271&text="'Bot start at '+timeo+'\n'+'time now is '+ttt)
     print(gg.text)
 
 while True:
